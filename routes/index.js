b39e342c6bb4a7b6589641cc890076612155a1fa
bitnami <bitnami@ubuntu.defaultdomain>
2016-12-09 16:33:47 +0000

    nueva estructura MVC

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controllerIndex = require('../controller/controllerIndex.js');
-
-/* GET home page. */
-router.get('/', controllerIndex.index);
-
-module.exports = router;
