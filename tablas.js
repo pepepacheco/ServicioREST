349bff93143bb58a150ad17a3999b2deb22eb3d8
Ubuntu <ubuntu@ip-172-31-29-142.us-west-2.compute.internal>
2017-01-03 12:46:06 +0000

    init branch

--- tablas.js
+++ tablas.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'default',
-    password: 'default',
+    user: 'root',
+    password: 'root',
     port: '3306',
     database: 'ServicioREST'
 });
